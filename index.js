0ed6de0b8840e4ea15ed18c17ad1646eac6e2f20
danny-pham <duongpq254@gmail.com>
2024-02-24 15:11:03 +0700

    add evm support

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const readline = require('readline');
-const generateSeedPhrase = require('./seed-phrase-generator');
+const { generateSeedPhrase, createEthereumWalletFromSeedPhrase } = require('./seed-phrase-generator');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-function createSeedPhrases(numberOfPhrases, phraseLength) {
-  if (isNaN(numberOfPhrases) || numberOfPhrases <= 0) {
-    console.error('Invalid number of phrases. Please provide a positive integer.');
-    rl.close();
-    return;
-  }
-
-  if (phraseLength !== 12 && phraseLength !== 24) {
-    console.error('Invalid phrase length. Please provide 12 or 24.');
-    rl.close();
-    return;
-  }
-
-  for (let i = 0; i < numberOfPhrases; i++) {
-    const seedPhrase = generateSeedPhrase(phraseLength);
-    console.log(`Generated Seed Phrase ${i + 1} (${phraseLength} words):`, seedPhrase);
-  }
-
-  rl.close();
-}
 
 rl.question('Enter the number of seed phrases you want to generate: ', (numberOfPhrasesInput) => {
   const numberOfPhrases = parseInt(numberOfPhrasesInput, 10);
@@ -33,6 +12,19 @@ rl.question('Enter the number of seed phrases you want to generate: ', (numberOf
   rl.question('Enter the length of each seed phrase (12 or 24 words): ', (phraseLengthInput) => {
     const phraseLength = parseInt(phraseLengthInput, 10);
 
-    createSeedPhrases(numberOfPhrases, phraseLength);
+    const seedPhrases = [];
+    for (let i = 0; i < numberOfPhrases; i++) {
+      seedPhrases.push(generateSeedPhrase(phraseLength));
+    }
+
+    seedPhrases.forEach((seedPhrase, index) => {
+      console.log(`Seed Phrase ${index + 1}: ${seedPhrase}`);
+      const ethereumWallet = createEthereumWalletFromSeedPhrase(seedPhrase);
+      console.log(`Ethereum Wallet ${index + 1}:`);
+      console.log(`Address: ${ethereumWallet.address}`);
+      console.log(`Private Key: ${ethereumWallet.privateKey}\n`);
+    });
+
+    rl.close();
   });
 });
